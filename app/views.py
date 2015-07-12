53734bfff3374d24f5ba4d7f43779ba1aaeadf7c
Rob Bartee <developer@hillviewscouting.org>
2015-07-12 09:36:25 -0600

    Rewrote the menu bar to use boot strap menu items. Updated the routing to pass along which menu item should look activeated.

--- app/views.py
+++ app/views.py
@@ -4,16 +4,16 @@ from app import app
 
 @app.route('/')
 def landing():
-    return render_template('landing.html', title='Home')
+    return render_template('landing.html', title='Home', menu='home')
 
 @app.route('/troop40')
 def troop40():
-    return render_template('troop40.html', title='Troop 40')
+    return render_template('troop40.html', title='Troop 40', menu='troop')
 
 @app.route('/pack40')
 def pack40():
-    return render_template('pack40.html', title='Pack 40')
+    return render_template('pack40.html', title='Pack 40', menu='pack')
 
 @app.route('/crew40')
 def crew40():
-    return render_template('crew40.html', title='Crew 40')
+    return render_template('crew40.html', title='Crew 40', menu='crew')
