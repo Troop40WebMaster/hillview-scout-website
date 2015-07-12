3320a590c75e7e69db96db974d33a0e4c975106e
Rob Bartee <developer@hillviewscouting.org>
2015-07-12 16:28:11 -0600

    o Added a title page that displays the logos according to which subpage we are on.

--- app/views.py
+++ app/views.py
@@ -4,16 +4,16 @@ from app import app
 
 @app.route('/')
 def landing():
-    return render_template('landing.html', title='Home', menu='home')
+    return render_template('landing.html', title='Home', subpage='home')
 
 @app.route('/troop40')
 def troop40():
-    return render_template('troop40.html', title='Troop 40', menu='troop')
+    return render_template('troop40.html', title='Troop 40', subpage='troop')
 
 @app.route('/pack40')
 def pack40():
-    return render_template('pack40.html', title='Pack 40', menu='pack')
+    return render_template('pack40.html', title='Pack 40', subpage='pack')
 
 @app.route('/crew40')
 def crew40():
-    return render_template('crew40.html', title='Crew 40', menu='crew')
+    return render_template('crew40.html', title='Crew 40', subpage='crew')
